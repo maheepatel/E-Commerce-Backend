ac06f78e10f86d2ddfee079075f37613bc19ef3b
maheepatel <mahendarptl9@gmail.com>
2025-04-24 23:47:33 +0530

    "ADD: order controller functions. "

---
+++ controllers/orderControllers.js
@@ -0,0 +1,21 @@
+
+// Placing orders using COD(cash on delivery) method
+
+const placeOrder = async (req, res) => {};
+
+// Placing orders using Stripe method
+
+const placeOrderStripe = async (req, res) => {};
+
+// Placing orders using Razorpay method
+
+const placeOrderRazorpay = async (req, res) => {};
+
+// All orders data for Admin panel
+const allOrders = async (req, res) => {};
+
+// User orders data for Frontend
+const userOrders = async (req, res) => {};
+
+// Update order status from Admin panel
+const updateStatus = async (req, res) => {};
