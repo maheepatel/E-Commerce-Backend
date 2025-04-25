fd9d769bc2e6d8363be22200132b44cee4643c31
maheepatel <mahendarptl9@gmail.com>
2025-04-25 23:26:06 +0530

    "ADD: Place order logic done.(Not tested)"

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,6 +1,38 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
 // Placing orders using COD(cash on delivery) method
 
-const placeOrder = async (req, res) => {};
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      data: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    // clear cart data once order is placed
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+    res.status(200).json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: `Order was not placed because of ${error.message}`,
+      });
+  }
+};
 
 // Placing orders using Stripe method
 
