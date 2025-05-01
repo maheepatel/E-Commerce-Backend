a5ffab7c17035047752623a7d9352573e7dddbc5
maheepatel <mahendarptl9@gmail.com>
2025-05-01 22:21:25 +0530

    "UPDATE: Implement Stripe payment method for placing orders with delivery charges in orderController."

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,5 +1,14 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+// global variables
+const currency = "usd";
+const deliveryCharges = 10;
+
+// Gateway initialization
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing orders using COD(cash on delivery) method
 
@@ -35,7 +44,63 @@ const placeOrder = async (req, res) => {
 
 // Placing orders using Stripe method
 
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: deliveryCharges * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.status(200).json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: `Order was not placed because of ${error.message}`,
+    });
+  }
+};
 
 // Placing orders using Razorpay method
 
